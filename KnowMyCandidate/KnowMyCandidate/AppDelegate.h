d7d4d4d8e72a53b1f2764b9f75dab9c48d78cc7c
Samir C <samirchoudhary@ucla.edu>
2015-10-13 12:08:35 -0700

    Adding iOS skeleton

---
+++ KnowMyCandidate/KnowMyCandidate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnowMyCandidate
+//
+//  Created by Samir Choudhary on 10/13/15.
+//  Copyright © 2015 Samir Choudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
