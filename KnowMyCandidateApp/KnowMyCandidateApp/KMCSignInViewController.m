843aae0f2cb4b2f6724e99b24030f3362542ea5f
Samir C <samirchoudhary@ucla.edu>
2015-10-17 18:12:38 -0700

    Added FB SDK integration

--- KnowMyCandidateApp/KnowMyCandidateApp/KMCSignInViewController.m
+++ KnowMyCandidateApp/KnowMyCandidateApp/KMCSignInViewController.m
@@ -1,6 +1,9 @@
 #import "KMCSignInViewController.h"
 
-#import <Parse/Parse.h>
+#import "FBSDKCoreKit/FBSDKCoreKit.h"
+#import "FBSDKLoginKit/FBSDKLoginKit.h"
+#import "Parse/Parse.h"
+#import "ParseFacebookUtilsV4/PFFacebookUtils.h"
 
 @interface KMCSignInViewController ()
 @end
@@ -9,6 +12,28 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  self.fields = PFLogInFieldsFacebook | PFLogInFieldsUsernameAndPassword | PFLogInFieldsLogInButton;
+  [self.logInView.facebookButton addTarget:self
+                                    action:@selector(didTapFbLoginButton)
+                          forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)didTapFbLoginButton {
+  // Set permissions required from the user's FB account.
+  NSArray *permissionsArray = @[ @"user_about_me" ];
+
+  // Login PFUser using Facebook
+  [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray
+                                                  block:^(PFUser *user, NSError *error) {
+    if (!user) {
+      NSLog(@"Uh oh. The user cancelled the Facebook login.");
+    } else if (user.isNew) {
+      NSLog(@"User signed up and logged in through Facebook!");
+    } else {
+      NSLog(@"User logged in through Facebook!");
+    }
+  }];
 }
 
 @end
