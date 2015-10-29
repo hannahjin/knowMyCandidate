392576a97fe1d5d1b871ed1cf42e41955b0da904
Jennifer Huang <jennhuang8@gmail.com>
2015-10-28 21:54:44 -0700

    Revert "Check for duplicate news & delete old news"
    This reverts commit e3effd01847146aaafe3d64015754e18d371d48a.

--- WebScrapingScripts/src/com/knowMyCandidate/WebParser.java
+++ WebScrapingScripts/src/com/knowMyCandidate/WebParser.java
@@ -5,11 +5,8 @@ public class WebParser {
     public static void main(String[] args) {
 //        ParserContext parserContext1 = new ParserContext(new PositionOnIssuesParser());
 //        parserContext1.executeStrategy();
-        //ParserContext parserContext2 = new ParserContext(new CandidateProfileParser());
-        //parserContext2.executeStrategy();
-        ParserContext parserContext3 = new ParserContext(new CalendarEventsParser());
-        parserContext3.executeStrategy();
-        
+        ParserContext parserContext2 = new ParserContext(new CandidateProfileParser());
+        parserContext2.executeStrategy();
     }
 }
 
