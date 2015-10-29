4184d15be5b026fa96dd576bcbb55f939575fba6
Jennifer Huang <jennhuang8@gmail.com>
2015-10-28 21:56:28 -0700

    Check for duplicate news & delete old news

--- WebScrapingScripts/bing_search.py
+++ WebScrapingScripts/bing_search.py
@@ -11,27 +11,30 @@ API_KEY = 'Cfd+0huY6d7Ekhz04lEXQfJBr2jt5AS3Oi323X8TtV4'
 
 def main():
     # TODO(jennhuang): Given a list of candidates, perform a search query for each candidate.    
-    bing_search("hillary clinton")
+    bing_search('martin o\'malley')
  
 def bing_search(query):
     escaped_query = urllib.quote(query)
     # create credential for authentication
     credentials = (':%s' % API_KEY).encode('base64')[:-1]
+    
     auth = 'Basic %s' % credentials
     url = 'https://api.datamarket.azure.com/Bing/Search/v1/News'+'?Query=%27'+ escaped_query+'%27&$top=5&$format=json'
+    
     request = urllib2.Request(url)
     request.add_header('Authorization', auth)
     request_opener = urllib2.build_opener()
     response = request_opener.open(request) 
     response_data = response.read()
     json_result = json.loads(response_data)
+    print json_result
     result_list = json_result['d']['results']
-    for article in result_list:
+    #for article in result_list:
         # TODO(jennhuang): Instead of printing out the articles, put the articles in the database
-        print article['Title']
-        print article['Description']
-        print article['Source']
-        print article['Url']
+        #print article['Title']
+        #print article['Description']
+        #print article['Source']
+        #print article['Url']
  
 if __name__ == "__main__":
     main()
