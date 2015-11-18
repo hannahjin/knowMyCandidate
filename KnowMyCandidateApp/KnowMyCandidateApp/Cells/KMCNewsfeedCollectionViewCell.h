49b58e81dada8ccd55bfc641879e8ad2c42759c8
Samir C <samirchoudhary@ucla.edu>
2015-11-17 21:19:25 -0800

    merging

---
+++ KnowMyCandidateApp/KnowMyCandidateApp/Cells/KMCNewsfeedCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KMCNewsfeedCollectionViewCell.h
+//  KnowMyCandidateApp
+//
+//  Created by Samir Choudhary on 11/17/15.
+//  Copyright © 2015 Samir Choudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMCNewsfeedCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ KnowMyCandidateApp/KnowMyCandidateApp/Cells/KMCNewsfeedCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  KMCNewsfeedCollectionViewCell.m
+//  KnowMyCandidateApp
+//
+//  Created by Samir Choudhary on 11/17/15.
+//  Copyright © 2015 Samir Choudhary. All rights reserved.
+//
+
+#import "KMCNewsfeedCollectionViewCell.h"
+
+@implementation KMCNewsfeedCollectionViewCell
+
+@end
